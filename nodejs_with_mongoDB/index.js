fbc3048968555709a2e202b28f7ea7926424473e
Zunayed Ebney Faruk <122368527+MdZunaed@users.noreply.github.com>
2024-09-21 21:58:34 +0600

    connect mongo db & insert data

---
+++ nodejs_with_mongoDB/index.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb+srv://mdzunaed8:9gapDHyss2i3CCxS@cluster0.natdh.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Successfully connected to Atlas");
+        var dbo = client.db("school");
+
+        var myobj = { name: "Zunayed", roll: 450197, address: "Dhaka" };
+        await dbo.collection("students").insertOne(myobj, function (err, res) {
+            if (err) {
+                console.error("Insert error: ", err);
+                throw err;
+            }
+
+            console.log("1 document inserted");
+            client.close();
+        });
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
