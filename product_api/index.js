572b0342d561ee72d5a9bf6607351a2cc7bf1b61
Zunayed <mdzunaed4@gmail.com>
2025-04-02 19:44:56 +0600

    product by id

--- product_api/index.js
+++ product_api/index.js
@@ -1,19 +1,33 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
+const { json } = require('stream/consumers');
 
 let server = http.createServer((req, res) => {
-    console.log(req.method + '->' + req.url + ' ========>');
+    let parsedUrl = url.parse(req.url, true);
+    console.log(`${req.method} -> ${parsedUrl.pathname} ========>`);
 
-    if (req.url == "/products" && req.method == "GET") {
-        fs.readFile("./products.json", "utf-8",
-            (error, data) => {
-                if (error == null) {
-                    res.end(data);
-                } else {
-                    fs.end(error);
-                }
-            }
-        );
+    let products = fs.readFileSync("./products.json", 'utf-8');
+
+    if (parsedUrl.pathname == "/products" && req.method == "GET" && parsedUrl.query.id == undefined) {
+        res.end(products != undefined ? products : "No Products");
+    } else if (parsedUrl.pathname == "/products" && req.method == "GET" && parsedUrl.query.id != undefined) {
+        let productsArray = JSON.parse(products);
+        let product = productsArray.find((product) => {
+            return product.id == parsedUrl.query.id;
+        });
+
+        // if(product != undefined){
+
+        // res.end(JSON.stringify(product));
+        // } else{
+        //     res.end(JSON.stringify({
+        //         'message': "Not found!"
+        //     }));
+        // }
+
+        res.end(product != undefined ? JSON.stringify(product) : "Not found");
+
     }
     else {
         res.end('Not Found');
