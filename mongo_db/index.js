cdd7d36026081bc35dd28f61e29909310c0be03b
Zunayed <mdzunaed4@gmail.com>
2025-04-06 23:22:02 +0600

    mongo db

---
+++ mongo_db/index.js
@@ -0,0 +1,20 @@
+// -To show DBs present in system
+// show dbs
+
+// -To create a new DB / Switch DB
+// use db_name
+
+// -To create a new collection
+// db.createCollection(‘name’)
+
+// -To view the collection in DB
+// show collections
+
+// -To create a new document in collection
+// db.collection_name.insertOne( {})
+
+// -To create multiple document in collection
+// db.collection_name.insertMany( [ {}, {} ] )
+
+// -To view document in collection
+// db.collection_name.find()
