09a483ac0dde1002da1623537d4e5825edaba134
Zunayed <mdzunaed4@gmail.com>
2025-04-09 23:49:49 +0600

    db insert, fetch

--- mongo_db/index.js
+++ mongo_db/index.js
@@ -1,3 +1,88 @@
+const mongoose = require('mongoose');
+
+let db = mongoose.connect("mongodb://localhost:27017/nodejs").then(() => {
+    console.log("Connection Successful");
+
+}).catch((error) => {
+    console.log("nongoDB connection error=======>", error);
+});
+
+// Schema
+const userSchema = mongoose.Schema(
+
+    {
+        name: {
+            type: String,
+            required: [true, "Name is required"]
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            minLength: [6, "Password should be minimum 6 character"],
+            maxLength: [15, "Password should be maximum 15 character"],
+        },
+        age: {
+            type: Number,
+            min: [12, "Minimum age is 12"],
+            max: [80, "Minimum age is 80"]
+        },
+        role: {
+            type: String,
+            enum: {
+                values: ["user", "vendor", "rider"],
+                message: "{VALUE} not allowed, Only user, vendor, rider is allowed"
+            }
+        }
+    },
+    // {
+    //     timestamps: true, // adds createdAt and updatedAt
+    // },
+);
+
+// Model
+
+const userModel = mongoose.model("users", userSchema);
+
+let user = {
+    name: "Zunayed",
+    password: "password",
+    age: 21,
+    role: "user",
+}
+
+// userModel.create(user).then((data) => {
+//     console.log(data);
+//     console.log("Data Inserted");
+//     mongoose.disconnect();
+// }).catch((error) => console.log(error));
+
+userModel.find().then((data) => {
+    console.log(data);
+    mongoose.disconnect();
+}).catch((error) => console.log(error));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // -To show DBs present in system
 // show dbs
 
