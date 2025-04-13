d2151be54e29396032b7f3b6be5516eb5048ae12
Zunayed <mdzunaed4@gmail.com>
2025-04-13 13:33:11 +0600

    express post, put, delete

--- express/index.js
+++ express/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/products", (req, res) => {
     res.send({ message: "Products from express", data: [] });
 });
@@ -18,6 +20,23 @@ app.get("/products/:id", (req, res) => {
 app.get("/login/:id", middleware, (req, res) => {
     res.send("Login Succesful");
 });
+
+app.post("/products", (req, res) => {
+    res.send({ message: "Post is working", data: req.body });
+});
+
+app.delete("/products/:id", (req, res) => {
+    res.send({
+        id: parseInt(req.params.id),
+        message: "product deleted"
+    });
+});
+
+
+app.put("/products/:id", (req, res) => {
+    res.send({ id: parseInt(req.params.id), message: "Product updated", data: req.body });
+});
+
 
 function middleware(req, res, next) {
     if (req.params.id < 18) {
