ce97dd6c8357c4db1226e71e2ad77f279bfeba45
Zunayed <mdzunaed4@gmail.com>
2025-04-13 23:33:10 +0600

    express mongo - CRUD

--- express/index.js
+++ express/index.js
@@ -1,20 +1,66 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.json());
 
+// Database Connection
+mongoose.connect("mongodb://localhost:27017/nodejs").then(() => {
+    console.log("Database connected");
+}).catch((error) => console.log(error));
+
+// Schema of Product
+
+const productSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+        },
+        price: {
+            type: Number,
+            min: [0, "Price can't be negetive"],
+            required: [true, "Price is required"]
+        },
+        quantity: {
+            type: Number,
+            required: [true, "Quantity is required"],
+        },
+        category: {
+            type: String,
+            enum: ["Clothing", "Electronics", "Household"]
+        },
+    },
+    { timestamps: true }
+);
+
+// Model for Product
+
+const productModel = mongoose.model("products", productSchema);
+
 app.get("/products", (req, res) => {
-    res.send({ message: "Products from express", data: [] });
+    productModel.find().then((products) => {
+        res.send({
+            message: "Success", data: products
+        });
+    }).catch((error) => {
+        console.log(error);
+        res.send({ message: "Something went wrong! Product not found" });
+    });
 });
 
 app.get("/products/:id", (req, res) => {
-    res.send({
-        message: "Single product", data: [{
-            id: parseInt(req.params.id),
-            name: "dummy"
-        }]
+    productModel.find({ _id: req.params.id }).then((product) => {
+        res.send({
+            message: "Success",
+            data: product
+        });
+    }).catch((error) => {
+        console.log(error);
+        res.send({ message: "Something went wrong! Product not found" });
     });
+
 });
 
 app.get("/login/:id", middleware, (req, res) => {
@@ -22,19 +68,38 @@ app.get("/login/:id", middleware, (req, res) => {
 });
 
 app.post("/products", (req, res) => {
-    res.send({ message: "Post is working", data: req.body });
+    productModel.create(req.body)
+        .then((doc) => {
+            res.send({ message: "Success", data: doc });
+        }).catch((error) => {
+            console.log(error);
+            res.send({ message: "Something went wrong! Product not added" });
+        });
 });
 
 app.delete("/products/:id", (req, res) => {
-    res.send({
-        id: parseInt(req.params.id),
-        message: "product deleted"
+    productModel.deleteOne({ _id: req.params.id }).then((info) => {
+        res.send({
+            message: "Success",
+            data: info
+        });
+    }).catch((error) => {
+        console.log(error);
+        res.status(400).send({ message: "Something went wrong! Product not deleted" });
     });
 });
 
 
 app.put("/products/:id", (req, res) => {
-    res.send({ id: parseInt(req.params.id), message: "Product updated", data: req.body });
+    productModel.updateOne({ _id: req.params.id }, req.body).then((info) => {
+        res.send({
+            message: "Success",
+            data: info
+        });
+    }).catch((error) => {
+        console.log(error);
+        res.status(400).send({ message: "Something went wrong! Product not Updated" });
+    });
 });
 
 
